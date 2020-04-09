8baeef2214c91b3dac0777d2a70bbd234338e11c
Sarath Yasodharan <sarath@iisc.ac.in>
2020-04-09 04:58:50 +0000

    Code clean up.

--- staticInst/validation.py
+++ staticInst/validation.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
 """"
 Copyright [2020] [Indian Institute of Science, Bangalore]
 SPDX-License-Identifier: Apache-2.0
@@ -32,11 +31,27 @@ def travel_distance_distribution(m_min,m_max,a,b):
     temp = np.array(temp)
     return temp/np.sum(temp)
 
-def validate_distributions(ageDistribution,householdDistribution,schoolsizeDistribution):
-# age distribution
+def validate_distributions(city):
+    # read parameters based on city
+    cityprofile = pd.read_json('./data/base/'+city+'/cityProfile.json')
+    m_max_commuter_distance = cityprofile['maxWorkplaceDistance'].values[1]
+    ageDistribution = cityprofile['age']['weights']
+    householdSizes = cityprofile['householdSize']['bins']
+    householdDistribution = cityprofile['householdSize']['weights']
+    
+    a_workplacesize = 3.26
+    c_workplacesize = 0.97
+    m_max_workplacesize = 2870
+    
+    a_commuter_distance = 4
+    b_commuter_distance = 3.8 
+    m_max_commuter_distance = 130
+ 
+    # create age values and expand the age distirution
     age_values = np.arange(0,81,1)
     age_distribution_over_gap5 = ageDistribution
 
+    age_distribution_over_gap5 = ageDistribution
     age_distribution = []
     for i in range(0,len(age_distribution_over_gap5)-1):
         for j in range(0,5):
@@ -45,21 +60,32 @@ def validate_distributions(ageDistribution,householdDistribution,schoolsizeDistr
     age_distribution = np.array(age_distribution)
     age_distribution = age_distribution/sum(age_distribution)
     
-    household_sizes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-    household_dist = householdDistribution  #1,2,3,4,5,6,7-10,11-14,15+
-    household_distribution = household_dist[0:6]
-    for i in range(0,4):
-        household_distribution.append(household_dist[6]/4)
-    for i in range(0,4):
-        household_distribution.append(household_dist[7]/4)
-    household_distribution.append(household_dist[8])
-    household_distribution = np.array(household_distribution)/np.sum(household_distribution)
-    # generate s sample of size of workplace 
-    a=3.26
-    c=0.97
-    m_max=2870
-        #function to generate a truncated Zipf sample
+    # create household sizes and spread the distribution if needed
+    household_sizes = [] 
+    household_dist = []
+    for i in range(len(householdSizes)):
+        size = householdSizes[i]
+        if "-" in size:
+            size = size.split('-')
+            diff = int(size[1]) - int(size[0]) + 1 #inclusive difference where you want the first number to be present
+            for j in range(diff):
+                household_sizes.append(int(size[0])+j)
+                household_dist.append(householdDistribution[i]/diff)
+        elif "+" in size: #last index
+            size = int(size.split("+")[0])
+            household_sizes.append(size)
+            household_dist.append(householdDistribution[i])
+        else:
+            household_sizes.append(int(householdSizes[i]))
+            household_dist.append(householdDistribution[i])
+
+    household_distribution = np.array(household_dist)/np.sum(household_dist)
     
+
+    # generate workplace size distribution
+    a=a_workplacesize
+    c=c_workplacesize
+    m_max=m_max_workplacesize    
     workplace_sizes = np.arange(m_max)
     p_nplus = np.arange(float(m_max))
     for m in range(m_max):
@@ -71,52 +97,33 @@ def validate_distributions(ageDistribution,householdDistribution,schoolsizeDistr
     for m in range(1, m_max):
         p_n[m] = p_nminus[m] - prev
         prev = p_nminus[m] 
-    
-    bzipf = stats.rv_discrete (name='bzipf', values=(workplace_sizes, p_n))
-    
-
-    individuals = pd.read_json('./data/bangalore-100K-300students/individuals.json')
-    
-    age = {'id':np.unique(individuals['age'].values), 'number of people':[0 for i in range(0,len(np.unique(individuals['age'].values)))]}
-    age = pd.DataFrame(age)
-    x = np.unique(individuals['household'].values)
-    x = x[~np.isnan(x)]
-    households = {'id':x, 'number of people': [0 for i in range(0,len(x))] }
-    households = pd.DataFrame(households)
-    
-    x = np.unique(individuals['school'].values)
-    x = x[~np.isnan(x)]
-    schools = {'id':x, 'number of people': [0 for i in range(0,len(x))] }
-    schools = pd.DataFrame(schools)
-    
-    x = np.unique(individuals['workplace'].values)
-    x = x[~np.isnan(x)]
-    
-    workplaces = {'id':x, 'number of people': [0 for i in range(0,len(x))]}
-    workplaces = pd.DataFrame(workplaces)
-    workplace_distances = []    
-
-    wp = pd.read_json('./data/bangalore-100K-300students/workplaces.json')
-    
-    for i in range(0,len(individuals)):
-        print(i/len(individuals))
-        age.at[age['id']==individuals.loc[i,'age'],'number of people'] = 1+age.loc[age['id']==individuals.loc[i,'age'],'number of people']
         
-        households.at[households['id']==individuals.loc[i,'household'],'number of people'] = 1+households.loc[households['id']==individuals.loc[i,'household'],'number of people']
+    # read data frames for validation
+    individuals = pd.read_json('./data/'+city+'-100K-300students/individuals.json')
+    wp = pd.read_json('./data/'+city+'-100K-300students/workplaces.json')
     
-        if not(np.isnan(individuals.loc[i,'school'])):
-            schools.at[schools['id']==individuals.loc[i,'school'],'number of people'] = 1+schools.loc[schools['id']==individuals.loc[i,'school'],'number of people']
-            
-        if not(np.isnan(individuals.loc[i,'workplace'])):
-            workplaces.at[workplaces.index==int(individuals.loc[i,'workplace']),'number of people'] = 1+workplaces.loc[workplaces.index==individuals.loc[i,'workplace'],'number of people']
-            workplace_distances.append(distance(individuals.loc[i,'lat'],individuals.loc[i,'lon'],wp.loc[wp.index==int(individuals.loc[i,'workplace']),'lat'],wp.loc[wp.index==int(individuals.loc[i,'workplace']),'lon']))
-    # plot age distribution
-    plt.plot(age['number of people'].values/(np.sum(age['number of people'].values)), 'r')
-    plt.plot(age_distribution)
+    # create empirical distributions from instantiated city
+    # age
+    age_output =  [ len(np.where(individuals['age'].values == i)[0]) for i in age_values]/np.sum([ len(np.where(individuals['age'].values == i)[0]) for i in age_values])    
+    # household size
+    full_frame = np.array([len(np.where(individuals['household'] == i)[0]) for i in np.unique(individuals['household'].values)])    
+    householdsize_output = [len(np.where(full_frame == j)[0]) for j in household_sizes]/np.sum([len(np.where(full_frame == j)[0]) for j in household_sizes])    
+    # workplace size
+    full_frame = np.array([len(np.where(individuals['workplace'] == i)[0]) for i in np.unique(individuals['workplace'].values)[~np.isnan(np.unique(individuals['workplace'].values))]])
+    workplacesize_output = [len(np.where(full_frame == j)[0]) for j in workplace_sizes] / np.sum([len(np.where(full_frame == j)[0]) for j in workplace_sizes])    
+    # commuter distance
+    full_frame = np.array([distance(individuals.loc[i,'lat'],individuals.loc[i,'lon'],wp.loc[wp.index==int(individuals.loc[i,'workplace']),'lat'],wp.loc[wp.index==int(individuals.loc[i,'workplace']),'lon']) for i in np.where(individuals['workplaceType']==1)[0]])        
+    commuter_distance_output = [len(np.where(np.array(np.floor(full_frame),dtype=int) ==i)[0]) for i in np.arange(0,m_max_commuter_distance)]/np.sum([len(np.where(np.array(np.floor(full_frame),dtype=int) ==i)[0]) for i in np.arange(0,m_max_commuter_distance)])
+    
+    # plots
+    
+    plt.plot(age_output, 'r',label='Instantiation')
+    plt.plot(age_distribution,label='Data')
     plt.xlabel('Age')
     plt.ylabel('Density')
     plt.title('Distribution of age')
     plt.grid(True)
+    plt.legend()
     plt.xticks(np.arange(0,81,10), np.concatenate((age_values[np.arange(0,71,10)], ['80+'])) )
     plt.savefig('age')
     plt.show()
@@ -125,60 +132,47 @@ def validate_distributions(ageDistribution,householdDistribution,schoolsizeDistr
     
     # plot household size distribution
     #HH_ranges = ['1','2','3','4','5','6','7-10','11-14','15+']
-    HH_ranges = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-    HH_numbers = np.array([len(households.loc[households['number of people']==HH_ranges[i]]) for i in range(0,len(HH_ranges))])
-    HH_output_distribution = HH_numbers/sum(HH_numbers)
-    plt.plot(HH_ranges,household_distribution)
-    plt.plot(HH_ranges,HH_output_distribution,'r')
-    plt.xticks(np.arange(1,16,1), np.concatenate((np.array(household_sizes)[np.arange(0,14,1)], ['15+'])) )
+    plt.plot(household_sizes,householdsize_output,'r',label='Instantiation')
+    plt.plot(household_sizes,household_distribution,label='Data')
+    plt.xticks(household_sizes, np.concatenate((np.array(household_sizes)[np.arange(0,len(household_sizes)-1,1)], [str(household_sizes[-1])])) )
     plt.xlabel('Household size')
     plt.ylabel('Density')
+    plt.legend()
     plt.title('Distribution of household size')
     plt.grid(True)
     plt.savefig('household_size')
     plt.show()
     plt.close()
     
-    WP_ranges = workplace_sizes
-    WP_numbers = np.array([len(workplaces.loc[workplaces['number of people']==WP_ranges[i]]) for i in range(0,len(WP_ranges))])
-    #WP_capacity = np.array([len(workplaces.loc[workplaces['capacity']==WP_ranges[i]]) for i in range(0,len(WP_ranges))])
-    #WP_capacity = WP_capacity/sum(WP_capacity)
-    WP_output_distribution = WP_numbers/sum(WP_numbers)
+    
     workplace_distribution = p_n
-    plt.plot(np.log(workplace_sizes), np.log(workplace_distribution))
-    plt.plot(np.log(workplace_sizes),np.log(WP_output_distribution),'r')
+    plt.plot(np.log10(workplace_sizes),np.log10(workplacesize_output),'r',label='Instantiation')
+    plt.plot(np.log10(workplace_sizes), np.log10(workplace_distribution),label='Model')
     plt.xlabel('Workplace size (log-scale)')
-    plt.ylabel('log-Density')
+    plt.ylabel('log_10 Density')
     plt.title('Distribution of workplace size (in log-scale)')
     plt.grid(True)
+    plt.legend()
     plot_xlabel =  [1, 10, 100, 1000, 2400]
-    #plot_ylabel = [1, 5, 25,  125, 625, 1000, 2400]
-    plot_xlabel1 = np.log(workplace_sizes)[plot_xlabel]
-    #plot_ylabel1 = np.log(workplace_distribution[plot_ylabel])
-    #plt.yticks(plot_ylabel1, workplace_distribution[plot_xlabel])
+    plot_xlabel1 = np.log10(workplace_sizes)[plot_xlabel]
     plt.xticks(plot_xlabel1, (workplace_sizes)[plot_xlabel])
     plt.savefig('workplace_size')
     plt.show()
     plt.close()
     
-    workplace_distance_empirical =  np.zeros((1,len(np.arange(m_min,m_max,1))))[0]
-    for j in range(0,len(workplace_distances)):
-        workplace_distance_empirical[int(workplace_distances[j])]+=1
-    workplace_distance_empirical = workplace_distance_empirical/np.sum(workplace_distance_empirical)
     actual_dist=[]
-    actual_dist = travel_distance_distribution(m_min,m_max,4,3.8)
-    d = np.arange(0,35,1)
-    plt.plot(np.log10(d),np.log10(actual_dist))
-    plt.plot(np.log10(d),np.log10((workplace_distance_empirical)),'r')
+    actual_dist = travel_distance_distribution(0,m_max_commuter_distance,a_commuter_distance,b_commuter_distance)
+    d = np.arange(0,m_max_commuter_distance,1)
+    plt.plot(np.log10(d),np.log10(actual_dist),'bo-',label='Model')
+    plt.plot(np.log10(d),np.log10((commuter_distance_output)),'ro-',label='Instantiation')
+    #plt.plot(np.log10(d),np.log10((distances)),'go-',label='Data')
     plt.xlabel('Workplace distance (km) (log-scale)')
-    plt.ylabel('log-Density')
+    plt.ylabel('log_10 Density')
     plt.title('Distribution of workplace distances')
-    plot_xlabel=[1,5,25,34]
+    plot_xlabel=[1,5,25,31]
     plot_xlabel1 = np.log10(d)[plot_xlabel]
     plt.xticks(plot_xlabel1,d[plot_xlabel])
     plt.grid(True)
+    plt.legend()
     plt.savefig('workplace_distance')
-    plt.show()
     plt.close()
-    
-    # create workplace distances
